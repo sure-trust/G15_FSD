6894fef8afca7ceb7092d9e325dcdabe121102e3
pratyusha-ravilla <pratyusharavilla123@gmail.com>
2023-09-13 16:17:59 +0530

    update

--- Mini Projects/PRATYUSHA RAVILLA/JavaScript Assignments/functionalprogrammingassesment/question-17.js
+++ Mini Projects/PRATYUSHA RAVILLA/JavaScript Assignments/functionalprogrammingassesment/question-17.js
@@ -10,22 +10,3 @@ const greeting = (arg1, arg2) => {
 
 
 
-// //question 16
-//   const greeting = (arg1, arg2) => {
-//     if (arg1 === undefined || arg1 === null) {
-//         throw new Error("arg1 is missing or null");
-//       }
-      
-//       return `${arg1} is from ${arg2}`;
-    
-    
-//     try {
-//       const message1 = greeting("Alice", "New York");
-//       console.log(message1); // Output: Alice is from New York
-      
-//       const message2 = greeting(null, "London"); // This will throw an error
-//       console.log(message2);
-//     } catch (error) {
-//       console.error(error.message);
-//     }
-// };
